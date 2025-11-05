56500382b6e227ef575b7cde9bf5e9939a3e6596
ayman <aymanosmanyousif@gmail.com>
2025-11-05 19:22:31 +0000

    added copilot toggle on <leader>ct

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,16 +1,28 @@
 return {
-	{ "github/copilot.vim" },
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim", branch = "master" },
-		},
-		build = "make tiktoken",
-		opts = {
-			-- See Configuration section for options
-		},
-		keys = {
-			{ "<leader>cc", "<cmd>CopilotChat<cr>", desc = "Open Copilot Chat" },
-		},
-	},
+  {
+    "github/copilot.vim",
+    config = function()
+      -- Toggle Copilot on/off
+      vim.keymap.set("n", "<leader>ct", function()
+        if vim.g.copilot_enabled == 1 then
+          vim.cmd("Copilot disable")
+          print("🛑 Copilot disabled")
+        else
+          vim.cmd("Copilot enable")
+          print("✅ Copilot enabled")
+        end
+      end, { desc = "Toggle Copilot" })
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {},
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChat<cr>", desc = "Open Copilot Chat" },
+    },
+  },
 }
