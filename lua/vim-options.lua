b6d842bfd00c104c5a70dee5e95aa20cdaee0062
ayman <aymanosmanyousif@gmail.com>
2025-11-11 23:26:12 +0000

    changed cursor back to normal in edit mode

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,7 +13,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
-vim.opt.guicursor = ""
 
 -- System clipboard integration
 vim.o.clipboard = "unnamedplus"
